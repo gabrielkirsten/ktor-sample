9e3b274623f1414636369b963c0d42ec0bb0e927
Gabriel Kirsten <gabriel.kirsten@hotmail.com>
2019-11-04 16:15:41 -0300

    Added hello world

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,25 @@
 package me.gabrielkirsten.example
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) { // Application Entry Point
+    val server = embeddedServer(Netty, 8080) { // Create an application using Netty
+        routing {// Install the Routing Feature and start configuring it
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText { "HELLO WORLD!" }
+            }
+        }
+    }
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+    server.start(wait = true) // start the server waiting for connections
 }
 
